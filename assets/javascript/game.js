50aa2bc8fb7fb902e98203edc6eea3ad93619e9f
Dylan Glover <dylanglover6@gmail.com>
2018-03-09 00:35:23 -0600

    added style and pause/play feature

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,16 @@
 var topic = ["Darth Vader", 
 "Chuck Norris", 
-"Batman", 
+"Captain America", 
 "The Power Rangers", 
 "Godzilla",
 "Indiana Jones",
 "Wonder Woman",
 "Abraham Lincoln",
-"Charizard"];
-var battleArray = [""];
+"Shrek",
+"Charizard",
+"Papa John",
+"Optimus Prime"];
+
 var firstFighter = 0;
 var secondFighter= 0;
 
@@ -41,39 +44,92 @@ $("button").on("click", function() {
           var p = $("<p>").text("Rating: " + rating);
 
           var personImage = $("<img>");
-          personImage.attr("src", results[i].images.fixed_height.url);
-        
+          personImage.attr("src", results[i].images.fixed_height_still.url);
+          personImage.attr("data-state", "still");
+          personImage.attr("class", "gif");
+          personImage.attr("data-still", results[i].images.fixed_height_still.url);
+              personImage.attr("data-animate", results[i].images.fixed_height.url);
           gifDiv.prepend(p);
           gifDiv.prepend(personImage);
 
           $("#character-1").prepend(gifDiv);
+          //play and pause
+          $(".gif").on("click", function() {
+
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });    
         }
-      });
+      
+    });
     }
+    //create second fighter
     else if (firstFighter != 0 && secondFighter === 0) {
             secondFighter = person
               $.ajax({
               url: queryURL,
               method: "GET"
             })
-              .then(function(response) {
-                var results = response.data;
+            .then(function(response) {
+            var results = response.data;
             
-                for (var i = 0; i < results.length; i++) {
-                  var gifDiv = $("<div class='item'>");
+            for (var i = 0; i < results.length; i++) {
+              var gifDiv = $("<div class='item'>");
         
-                  var rating = results[i].rating;
+              var rating = results[i].rating;
         
-                  var p = $("<p>").text("Rating: " + rating);
+              var p = $("<p>").text("Rating: " + rating);
         
-                  var personImage = $("<img>");
-                  personImage.attr("src", results[i].images.fixed_height.url);
-                
-                  gifDiv.prepend(p);
-                  gifDiv.prepend(personImage);
-        
-                  $("#character-2").prepend(gifDiv);
+              var personImage = $("<img>");
+              personImage.attr("src", results[i].images.fixed_height_still.url);
+              personImage.attr("data-state", "still");
+              personImage.attr("class", "gif");
+              personImage.attr("data-still", results[i].images.fixed_height_still.url);
+              personImage.attr("data-animate", results[i].images.fixed_height.url);
+              gifDiv.prepend(p);
+              gifDiv.prepend(personImage);
+                    
+              $("#character-2").prepend(gifDiv);
+                //play and pause
+              $(".gif").on("click", function() {
+
+                var state = $(this).attr("data-state");
+                if (state === "still") {
+                  $(this).attr("src", $(this).attr("data-animate"));
+                  $(this).attr("data-state", "animate");
+                } else {
+                  $(this).attr("src", $(this).attr("data-still"));
+                  $(this).attr("data-state", "still");
                 }
-              });
+              });   
+            }  
+            //winner function
+            if (firstFighter > secondFighter) {
+                var firstWin = $("<h2>");
+                firstWin.text("Winner: " + firstFighter );
+                $("#winner").append(firstWin);
             }
-  });
+            else if (firstFighter < secondFighter) {
+                var secondWin = $("<h2>");
+                secondWin.text("Winner: " + secondFighter);
+                $("#winner").append(secondWin);
+            }
+            else if (firstFighter === secondFighter) {
+                var tie = $("<h2>");
+                tie.text("Tie!!");
+                $("#winner").append(tie);
+            }
+        })
+        
+    };   
+      
+});
+
+
+    
